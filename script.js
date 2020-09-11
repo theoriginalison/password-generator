59eb5962fbafc586699323d218ab50a44c53f30f
Alison Lubar <aslubar@gmail.com>
2020-09-11 15:25:07 -0400

    added variables for password characters to include

--- script.js
+++ script.js
@@ -14,6 +14,20 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//need variables of choices
+
+var inputLength = 8;
+// OR var inputLength = Array(100).fill().map(function(v, i) { return i + 1; })
+var inputLowercase = //need range
+var inputUppercase = //need range
+var inputNumber = //need range
+var inputSpecial
+
+var inputLength = [];
+for (var i = 1; i <= 129; i++) {
+  numbers.push(i);
+}
+
 //button click needs to initiate first alert
 
 var pwdLength = prompt("How long should your password be?");
@@ -23,3 +37,7 @@ var yesNumbers = confirm("Would you like to include numbers?");
 var yesSpecial = confirm("Would you like to include special characters?");
 
 //for every "ok," need to use at least one character type-- bonus would be how many
+
+if inputLength //is less than 8 and greater than 128, reprompt, maybe w alert that says "please enter a number between 8-128"
+
+else //include at least one of whatever is chosen?
