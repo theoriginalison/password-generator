0906a9b5c7ba3c55016317f1b826daf4cc488864
Alison Lubar <aslubar@gmail.com>
2020-09-12 17:44:18 -0400

    added to README and cleaned up script.js by deleting most of the comments and unused/hypothetical script

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # password-generator
+
 Homework for Week 3
 
 [LINK TO PAGE](https:///)
@@ -13,11 +14,20 @@ _[Problems & Solutions](#changes)
 \_[Discoveries](#discoveries)
 
 ## Approach & Process
-- I first thought about the type of variables that I would need to create, and then realized that I would also need to create arrays that contained all of the possible characters.
-- Eventually, making an object seemed to make the most sense.
+
+- I wrote a TON of pseudocode, and tried to find pieces from Week 3's activities that matched. I used the RPS activity for quite a bit, and figured that I'd need to use Math.random at some point, and create arrays that included all of the possible characters (defining each as a variable by type).
+- I created boolean variables to correspond to alerts, and the password length needed to come first (and be a prompt instead). I knew that the length of the password that the user chose would be what all of the other characters are linked to, or depend on.
+- An object was created as another variable, to keep track of which specifications the user confirmed that they want in their password
+- Created variables for possible characters that included all of the arrays at the top, and guaranteed characters that selected from those randomly-- these two serve the functions of seeing all of the characters that the Math.random could choose from; for example, if the user selected to have no numbers, then the selections for the Math.random would be much fewer.
 
 ## Problems & Solutions
-- Wanting to have at least one of each character chosen by the user to be in a password. My initial inclination was to run a loop of whichever characters the user chose (the loop would select random characters from that array) over and over, until it reached a max of the number of characters set by the user in the prompt alert.
+
+- Wanting to have at least one of each character chosen by the user to be in a password. My initial inclination was to run a loop of whichever characters the user chose (the loop would select random characters from that array) over and over, until it reached a max of the number of characters set by the user in the prompt alert. However, I realized that by filling the password based on the boolean, any true choices would return at least one from that array.
 
 ## Discoveries
+
 - I might have actually pronounced concatenation correctly?!
+- Ask BCS & Tutoring was vital this week.
+- The .split("") function is very useful. But I could have also used global variables.
+- return is necessary to print something out in the actual page!
+- Console log any functions before you put the variables in, to make sure they're working the way that you want them to.
--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-var inputLowercase = "abcdefghijklmnopqrstuvwxyz".split(""); //move to global variables
+var inputLowercase = "abcdefghijklmnopqrstuvwxyz".split("");
 var inputUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var inputNumber = "0123456789".split("");
 var inputSpecial = "!@#$%^&*_-?~+".split("");
-var possiblePwds = {}; //this is the object?
-
-// Object.onclick = function (){};
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -17,6 +14,7 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
 function userInputFunction() {
   var pwdLength = prompt(
     "How many characters should your password be? Choose a length between 8 and 128."
@@ -34,23 +32,11 @@ function userInputFunction() {
   };
   return confirmChar;
 }
+
 function generatePassword() {
-  //need variables of choices, variables that include the entire array of possible options
   var userInput = userInputFunction();
   console.log(userInput);
-  // OR var inputLength = Array(100).fill().map(function(v, i) { return i + 1; }); these are used to generate the password:
 
-  var newPassword = "";
-
-  //button click needs to initiate first alert
-
-  //for every "ok," need to use at least one character type-- bonus would be how many
-  //don't need an object-- just look at the values of yesUpper and yesLower, pull the if statement inside the for loop and conditionally choose one of them
-
-  //NEXT: Loop through and guarantee one of each character
-  //repeats lower upper number special in a loop until it reaches pwdLength-- but also depends on what the user selects as wanting to be in; build one four-character pwd bc that also depends on what types of characters the user chooses
-  // how to make sure it includes at least one of each kind a "yes" is
-  // how to determine a length
   var possibleCharacters = [];
   var guaranteedCharacters = [];
   if (userInput.lower) {
@@ -73,29 +59,8 @@ function generatePassword() {
   }
 
   console.log(guaranteedCharacters);
-
-  // for (var i = 1; i < pwdLength; i++) {
-  //   numbers.push(i); //idk what this is
-  //   if (i === pwdLength - 4) break;
-
-  //   if (yesLowercase) {
-  //     possiblePwds.lower = inputLowercase;
-  //     //need to add
-
-  //     selLowercase =
-  //       possiblePwds[Math.floor(Math.random() * inputLowercase.length)];
-  //   } else yesLowercase.null; //doesn't run
-
-  //   if (yesUppercase) {
-  //     possiblePwds.upper = inputUppercase;
-  //   }
-  //   if (yesNumbers) {
-  //     possiblePwds.numbers = inputNumber;
-  //   }
-  //   if (yesSpecial) {
-  //     possiblePwds.special = inputSpecial;
-  //   }
-  // }
 }
+
+//something needs to go here so the guaranteedCharacters are printed in the box-- it is the #password in the HTML
 
 generateBtn.addEventListener("click", writePassword);
